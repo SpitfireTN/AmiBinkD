19ca493e475c6d217991ee7c00b70c3f6c809174
Gary T. McCulloch <61099952+SpitfireTN@users.noreply.github.com>
2026-01-31 07:23:52 -0500

    Cleaned structure. Amiga Ready.
    Removed header comments from ftnnode.c

--- ftnnode.c
+++ ftnnode.c
@@ -1,16 +1,3 @@
-/*
- *  ftnnode.c -- Handle our links
- *
- *  ftnnode.c is a part of binkd project
- *
- *  Copyright (C) 1996-1998  Dima Maloff, 5047/13
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version. See COPYING.
- */
-
 #include <stdlib.h>
 #include <string.h>
 
