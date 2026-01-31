b05e65f489ce26d9ab54fbe4354acfc040e8ee34
Gary T. McCulloch <61099952+SpitfireTN@users.noreply.github.com>
2026-01-31 07:13:38 -0500

    Remove copyright and license comments from ftnaddr.c
    Removed the header comment block from ftnaddr.c.

--- ftnaddr.c
+++ ftnaddr.c
@@ -1,15 +1,3 @@
-/*
- *  ftnaddr.c -- Handle FTN addresses
- *
- *  ftnaddr.c is a part of binkd project
- *
- *  Copyright (C) 1996,1997  Dima Maloff, 5047/13
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version. See COPYING.
- */
 
 #include <string.h>
 #include <ctype.h>
