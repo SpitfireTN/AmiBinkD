acb521b16955211dc34033b8ba21d4bd987ee61a
Gary T. McCulloch <61099952+SpitfireTN@users.noreply.github.com>
2026-01-31 06:42:01 -0500

    Refactor amiga_socket_init and amiga_socket_cleanup

--- amiga_glue.c
+++ amiga_glue.c
@@ -1,20 +1,32 @@
-#include <proto/exec.h>
-#include <proto/bsdsocket.h>
+/* amiga_glue.c - AmigaOS 3.x bsdsocket.library glue */
+
 #include <exec/types.h>
 #include <exec/libraries.h>
+
+#include <proto/exec.h>
+#include <proto/bsdsocket.h>
+
 #include <stdio.h>
 
 struct Library *SocketBase = NULL;
 
-int amiga_socket_init(void) {
+int amiga_socket_init(void)
+{
     SocketBase = OpenLibrary("bsdsocket.library", 4);
-    if (!SocketBase) {
+    if (SocketBase == NULL)
+    {
         printf("Unable to open bsdsocket.library\n");
         return -1;
     }
+
     return 0;
 }
 
-void amiga_socket_cleanup(void) {
-    if (SocketBase) CloseLibrary(SocketBase);
+void amiga_socket_cleanup(void)
+{
+    if (SocketBase != NULL)
+    {
+        CloseLibrary(SocketBase);
+        SocketBase = NULL;
+    }
 }
