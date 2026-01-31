64cda63bd07c741ff6b105c35e09cddbb56c7cb6
Gary T. McCulloch <61099952+SpitfireTN@users.noreply.github.com>
2026-01-31 07:31:17 -0500

    Correct and Amiga ready.
    Removed header comments from inbound.c

--- inbound.c
+++ inbound.c
@@ -1,15 +1,3 @@
-/*
- *  inbound.c -- inbound handling
- *
- *  inbound.c is a part of binkd project
- *
- *  Copyright (C) 1996-1997  Dima Maloff, 5047/13
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version. See COPYING.
- */
 
 #include <stdlib.h>
 #include <string.h>
