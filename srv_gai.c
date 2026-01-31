bef8fd7ea1d80ed80e04433adb255bbe1873abcd
Gary T. McCulloch <61099952+SpitfireTN@users.noreply.github.com>
2026-01-31 08:45:14 -0500

    Amiga ready - srv_gai.c
    Removed copyright notice and comments from the top of the file.

--- srv_gai.c
+++ srv_gai.c
@@ -1,26 +1,3 @@
-/*
- * srv_getaddrinfo.c -- SRV support for getaddrinfo
- *
- * Copyright 2011 Andre Grueneberg for BinkD
- *
- * inspired by getsrvinfo.c, Copyright 2009 Red Hat, Inc.
- * and by      ruli_getaddrinfo.c, Copyright (C) 2004 Goeran Weinholt
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; see the file COPYING.  If not, write to
- * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- */
-
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
